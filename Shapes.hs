8c67f0653d3821ee66cf8248d0c86aa4b45445fe
Zach <caday33@gmail.com>
2018-10-02 15:54:42 -0500

    added module description

--- Shapes.hs
+++ Shapes.hs
@@ -2,6 +2,13 @@
 -- Zachary Baklund, 2-10-18
 --
 
+module Shapes
+where
+
+-- exporting the functions area and perimeter, which apply to the data
+-- a Shape which is defined by an 'enum' and 1, 2, or 3 Floats
+-- 
+
 data Shape = Circle Float 
         | Rectangle Float Float
         | Triangle Float Float Float
