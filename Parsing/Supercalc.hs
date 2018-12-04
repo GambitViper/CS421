69de642e290e2d70378e02ccbfb016eb033182fb
Zach Baklund <baklund.zachary@uwlax.edu>
2018-12-03 19:41:24 -0600

    boolean evaluation upto relations and not

--- Parsing/Supercalc.hs
+++ Parsing/Supercalc.hs
@@ -1,32 +1,119 @@
 module Supercalc where
 
-import MonadicParser
 import Data.Char
-
-digit :: Parser Char Char
-digit = satisfy isDigit
-
+import MonadicParser
+           
+--
+-- Write a parser for expressions according to the grammar
+-- 
+-- Exprbool ::= Term ( OR Term )*
+-- Term ::= Factorbool ( AND Factorbool )*
+-- Factorbool ::= Bool | NOT Factorbool | '(' Exprbool ')'
+-- Bool ::= false | true | Relation
+-- Relation ::= Expr relation Expr
+--
+-- Expr ::= Factor ( ( '+' | '-' ) Factor )*
+-- Factor ::= Atomic ( ( '*' | '/' ) Atomic )*
+-- Atomic ::= Digit+ | '(' Expr ')'
+--
+           
 number :: Parser Char Int
-number = some digit `using` read
+number = some (satisfy isDigit) `using` read
 
-atomic = number `alt` (literal '(' `thn` \_ ->
-                       expr        `thn` \val ->
-                       literal ')' `thn` \_ -> accept val)
+boolt :: Parser Char Bool
+boolt = literal 't' `thn` \_ ->
+        literal 'r' `thn` \_ ->
+        literal 'u' `thn` \_ ->
+        literal 'e' `thn` \res ->
+        accept True
 
-multiplier = literal '*' `using` \_ -> (*)
-divider    = literal '/' `using` \_ -> div
-adder      = literal '+' `using` \_ -> (+)
-subtracter = literal '-' `using` \_ -> (-)
+boolf :: Parser Char Bool
+boolf = literal 'f' `thn` \_ ->
+        literal 'a' `thn` \_ ->
+        literal 'l' `thn` \_ ->
+        literal 's' `thn` \_ ->
+        literal 'e' `thn` \res ->
+        accept False
 
-factorSuffix :: Parser Char (Int -> Int -> Int, Int)
-factorSuffix = (multiplier `alt` divider) `thn` \op ->
-                atomic                    `thn` \num ->
-                accept (op, num)
+boolean :: Parser Char Bool
+boolean = boolt `alt` boolf
 
+booleval :: Parser Char Bool
+booleval = boolean `alt` relation
+
+-- Logical operators
+andop = literal 'a' `thn`   \_ ->
+        literal 'n' `thn`   \_ ->
+        literal 'd' `using` \_ -> (&&)
+orop  = literal 'o' `thn`   \_ ->
+        literal 'r' `using` \_ -> (||)
+notop = literal 'n' `thn`   \_ ->
+        literal 'o' `thn`   \_ ->
+        literal 't' `using` \_ -> (not)
+
+notSuffix :: Parser Char Bool
+notSuffix = notop `thn` \op ->
+            factorbool `thn` \f ->
+            accept(op f)
+
+factorbool :: Parser Char Bool
+factorbool = booleval `alt` notSuffix
+
+-- Relational operators
+-- Binary operators <, >, <=, >=, == and !=, which receive integers and return a boolean value
+lt  = literal '<' `using` \_ -> (<)
+gt  = literal '>' `using` \_ -> (>)
+lte = literal '<' `thn`   \_ ->
+      literal '=' `using` \_ -> (<=)
+gte = literal '>' `thn`   \_ ->
+      literal '=' `using` \_ -> (>=)
+eq  = literal '=' `thn`   \_ ->
+      literal '=' `using` \_ -> (==)
+neq = literal '!' `thn`   \_ ->
+      literal '=' `using` \_ -> (/=)
+
+relation :: Parser Char Bool
+relation = expr `thn` \val1 ->
+           (lt `alt` gt `alt` lte `alt` gte `alt` eq `alt` neq) `thn` \op ->
+           expr `thn` \val2 ->
+           accept (op val1 val2)
+           
+parened :: Parser Char Int
+parened = literal '(' `thn` \_ ->
+          expr        `thn` \res ->
+          literal ')' `thn` \_ ->
+          accept res
+
+parenedbool :: Parser Char Bool
+parenedbool = literal '(' `thn` \_ ->
+              exprbool    `thn` \res ->
+              literal ')' `thn` \_ ->
+              accept res
+           
+atomic :: Parser Char Int
+atomic = number `alt` parened
+           
+addition       = literal '+' `using` \_ -> (+)
+subtraction    = literal '-' `using` \_ -> (-)
+multiplication = literal '*' `using` \_ -> (*)
+division       = literal '/' `using` \_ -> (div)
+           
+factorSuffix :: Parser Char (Int -> Int)
+factorSuffix = (multiplication `alt` division) `thn` \op ->
+               atomic                          `thn` \val ->
+               accept (\z -> op z val)
+          
+factor :: Parser Char Int
+factor = atomic `thn` \num1 ->
+         many factorSuffix `thn` \suffixes ->
+         accept (foldl (\n f -> f n) num1 suffixes)
+           
+exprSuffix :: Parser Char (Int -> Int)
+exprSuffix = (addition `alt` subtraction) `thn` \op ->
+             factor                       `thn` \val ->
+             accept (\z -> op z val)
+           
 expr :: Parser Char Int
-expr = factor          `thn` \num1 ->
-       many exprSuffix `thn` \opvals ->
-       accept (let foldingFn :: Int -> (Int -> Int -> Int, Int) -> Int
-                   foldingFn x yz = case yz of
-                                    (op, x2) -> op x x2
-               in foldl foldingFn num1 opvals
+expr = factor `thn` \num1 ->
+       many exprSuffix `thn` \suffixes ->
+       accept (foldl (\n f -> f n) num1 suffixes)
