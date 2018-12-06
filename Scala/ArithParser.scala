5ca67d961e1f3515006885335c39180c58137589
Zach Baklund <baklund.zachary@uwlax.edu>
2018-12-06 17:50:33 -0600

    scala hw and ArithParser

---
+++ Scala/ArithParser.scala
@@ -0,0 +1,60 @@
+class ArithParser {
+  var idx: Int = 0
+  var str: String = null
+  
+  def parseExpr(input: String): Int = {
+    idx = 0
+    str = input
+    return expr()
+  }
+  def expr(): Int = {
+    var result: Int = term()
+    while(idx < str.length() && (str.charAt(idx) == '+' || str.charAt(idx) == '-')){
+      if(str.charAt(idx) == '+'){
+        idx += 1
+        result = result + term()
+      }else if(str.charAt(idx) == '-'){
+        idx += 1
+        result = result - term()
+      }else {
+        //failure
+      }
+    }
+    return result
+  }
+  def term(): Int = {
+    var result: Int = factor()
+    while(idx < str.length() && (str.charAt(idx) == '*' || str.charAt(idx) == '/')){
+      if(str.charAt(idx) == '*'){
+        idx += 1
+        result = result * factor()
+      }else if(str.charAt(idx) == '/'){
+        idx += 1
+        result = result / factor()
+      }else{
+        //failure
+      }
+    }
+    return result
+  }
+  def factor(): Int = {
+    var result: Int = 0
+    if(Character.isDigit(str.charAt(idx))){
+      result = str.charAt(idx).asDigit
+      idx += 1
+    }else if(str.charAt(idx) == '-'){
+      idx += 1
+      result = -expr()
+    }else if(str.charAt(idx) == '('){
+      idx += 1
+      result = expr()
+      if(str.charAt(idx) != ')'){
+        //failure
+      }
+      idx += 1
+    }else{
+      //failure
+    }
+    return result
+  }
+}
